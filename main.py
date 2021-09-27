5cea96e22fdea889ce767f276bc53a8cc5606ddd
Łukasz Ł <ukaszek1001@gmail.com>
2021-09-27 21:09:09 +0200

    dodano plik main.py

---
+++ main.py
@@ -0,0 +1,270 @@
+Styczen = 1.59282448436825
+Luty = -0.453509101198007
+Marzec = 2.32467171712441
+Kwiecien = 1.26125440724877
+Maj = 1.78252628571251
+Czerwiec = 2.32938454145522
+Lipiec = 1.50222984223283
+Sierpien = 1.78252628571251
+Wrzesien = 2.32884899407637
+Pazdziernik = 0.616921348207244
+Listopad = 2.35229588637833
+Grudzien = 0.337779545187098
+Styczen_2 = 1.57703524727525
+Luty_2 = -0.292781442607648
+Marzec_2 = 2.48619659017508
+Kwiecien_2 = 0.267110317834564
+Maj_2 = 1.41795267229799
+Czerwiec_2 = 1.05424326726375
+Lipiec_2 = 1.4805201044812
+Sierpien_2 = 1.57703524727525
+Wrzesien_2 = -0.0774206903147018
+Pazdziernik_2 = 1.16573339872354
+Listopad_2 = -0.404186717638335
+Grudzien_2 = 1.49970852083123
+
+
+kwota_kredytu = float(input("Podaj kwote kredytu: "))
+wysokosc_oprocentowania = float(input("Podaj kwote oprocentowania: "))
+wysokosc_raty = float(input("Podaj wysokosc raty: "))
+nazwa_miesiaca = input()
+inflacja = None
+pozostalo_kredytu = None
+
+# Styczen
+inflacja = Styczen
+
+pozostalo_kredytu = (1 +((inflacja + wysokosc_oprocentowania) / 1200)) * kwota_kredytu - wysokosc_raty
+tekst = "Twoja pozostala kwota kredytu to {}. To o {} mniej niz w poprzednim miesiacu"
+
+print(tekst.format(pozostalo_kredytu, kwota_kredytu - pozostalo_kredytu))
+kwota_kredytu = pozostalo_kredytu
+
+# Luty
+
+inflacja = Luty
+
+pozostalo_kredytu = (1 +((inflacja + wysokosc_oprocentowania) / 1200)) * kwota_kredytu - wysokosc_raty
+tekst = "Twoja pozostala kwota kredytu to {}. To o {} mniej niz w poprzednim miesiacu"
+
+print(tekst.format(pozostalo_kredytu, kwota_kredytu - pozostalo_kredytu))
+kwota_kredytu = pozostalo_kredytu
+
+# Marzec
+
+inflacja = Marzec
+
+pozostalo_kredytu = (1 +((inflacja + wysokosc_oprocentowania) / 1200)) * kwota_kredytu - wysokosc_raty
+tekst = "Twoja pozostala kwota kredytu to {}. To o {} mniej niz w poprzednim miesiacu"
+
+print(tekst.format(pozostalo_kredytu, kwota_kredytu - pozostalo_kredytu))
+kwota_kredytu = pozostalo_kredytu
+
+# Kwiecien
+
+inflacja = Kwiecien
+
+pozostalo_kredytu = (1 +((inflacja + wysokosc_oprocentowania) / 1200)) * kwota_kredytu - wysokosc_raty
+tekst = "Twoja pozostala kwota kredytu to {}. To o {} mniej niz w poprzednim miesiacu"
+
+print(tekst.format(pozostalo_kredytu, kwota_kredytu - pozostalo_kredytu))
+kwota_kredytu = pozostalo_kredytu
+
+# Maj
+
+inflacja = Maj
+
+pozostalo_kredytu = (1 +((inflacja + wysokosc_oprocentowania) / 1200)) * kwota_kredytu - wysokosc_raty
+tekst = "Twoja pozostala kwota kredytu to {}. To o {} mniej niz w poprzednim miesiacu"
+
+print(tekst.format(pozostalo_kredytu, kwota_kredytu - pozostalo_kredytu))
+kwota_kredytu = pozostalo_kredytu
+
+# Czerwiec
+
+inflacja = Czerwiec
+
+pozostalo_kredytu = (1 +((inflacja + wysokosc_oprocentowania) / 1200)) * kwota_kredytu - wysokosc_raty
+tekst = "Twoja pozostala kwota kredytu to {}. To o {} mniej niz w poprzednim miesiacu"
+
+print(tekst.format(pozostalo_kredytu, kwota_kredytu - pozostalo_kredytu))
+kwota_kredytu = pozostalo_kredytu
+
+# Lipiec
+
+inflacja = Lipiec
+
+pozostalo_kredytu = (1 +((inflacja + wysokosc_oprocentowania) / 1200)) * kwota_kredytu - wysokosc_raty
+tekst = "Twoja pozostala kwota kredytu to {}. To o {} mniej niz w poprzednim miesiacu"
+
+# Sierpien
+
+inflacja = Sierpien
+
+pozostalo_kredytu = (1 +((inflacja + wysokosc_oprocentowania) / 1200)) * kwota_kredytu - wysokosc_raty
+tekst = "Twoja pozostala kwota kredytu to {}. To o {} mniej niz w poprzednim miesiacu"
+
+print(tekst.format(pozostalo_kredytu, kwota_kredytu - pozostalo_kredytu))
+kwota_kredytu = pozostalo_kredytu
+
+# Wrzesien
+
+inflacja = Wrzesien
+
+pozostalo_kredytu = (1 +((inflacja + wysokosc_oprocentowania) / 1200)) * kwota_kredytu - wysokosc_raty
+tekst = "Twoja pozostala kwota kredytu to {}. To o {} mniej niz w poprzednim miesiacu"
+
+print(tekst.format(pozostalo_kredytu, kwota_kredytu - pozostalo_kredytu))
+kwota_kredytu = pozostalo_kredytu
+
+# Pazdziernik
+
+inflacja = Pazdziernik
+
+pozostalo_kredytu = (1 +((inflacja + wysokosc_oprocentowania) / 1200)) * kwota_kredytu - wysokosc_raty
+tekst = "Twoja pozostala kwota kredytu to {}. To o {} mniej niz w poprzednim miesiacu"
+
+print(tekst.format(pozostalo_kredytu, kwota_kredytu - pozostalo_kredytu))
+kwota_kredytu = pozostalo_kredytu
+
+# Listopad
+
+inflacja = Listopad
+
+pozostalo_kredytu = (1 +((inflacja + wysokosc_oprocentowania) / 1200)) * kwota_kredytu - wysokosc_raty
+tekst = "Twoja pozostala kwota kredytu to {}. To o {} mniej niz w poprzednim miesiacu"
+
+print(tekst.format(pozostalo_kredytu, kwota_kredytu - pozostalo_kredytu))
+kwota_kredytu = pozostalo_kredytu
+
+# Grudzien
+
+inflacja = Grudzien
+
+pozostalo_kredytu = (1 +((inflacja + wysokosc_oprocentowania) / 1200)) * kwota_kredytu - wysokosc_raty
+tekst = "Twoja pozostala kwota kredytu to {}. To o {} mniej niz w poprzednim miesiacu"
+
+print(tekst.format(pozostalo_kredytu, kwota_kredytu - pozostalo_kredytu))
+kwota_kredytu = pozostalo_kredytu
+
+# Styczen_2
+
+inflacja = Styczen_2
+
+pozostalo_kredytu = (1 +((inflacja + wysokosc_oprocentowania) / 1200)) * kwota_kredytu - wysokosc_raty
+tekst = "Twoja pozostala kwota kredytu to {}. To o {} mniej niz w poprzednim miesiacu"
+
+print(tekst.format(pozostalo_kredytu, kwota_kredytu - pozostalo_kredytu))
+kwota_kredytu = pozostalo_kredytu
+
+# Luty_2
+
+inflacja = Luty_2
+
+pozostalo_kredytu = (1 +((inflacja + wysokosc_oprocentowania) / 1200)) * kwota_kredytu - wysokosc_raty
+tekst = "Twoja pozostala kwota kredytu to {}. To o {} mniej niz w poprzednim miesiacu"
+
+print(tekst.format(pozostalo_kredytu, kwota_kredytu - pozostalo_kredytu))
+kwota_kredytu = pozostalo_kredytu
+
+# Marzec_2
+
+inflacja = Marzec_2
+
+pozostalo_kredytu = (1 +((inflacja + wysokosc_oprocentowania) / 1200)) * kwota_kredytu - wysokosc_raty
+tekst = "Twoja pozostala kwota kredytu to {}. To o {} mniej niz w poprzednim miesiacu"
+
+print(tekst.format(pozostalo_kredytu, kwota_kredytu - pozostalo_kredytu))
+kwota_kredytu = pozostalo_kredytu
+
+# Kwiecien_2
+
+inflacja = Kwiecien_2
+
+pozostalo_kredytu = (1 +((inflacja + wysokosc_oprocentowania) / 1200)) * kwota_kredytu - wysokosc_raty
+tekst = "Twoja pozostala kwota kredytu to {}. To o {} mniej niz w poprzednim miesiacu"
+
+print(tekst.format(pozostalo_kredytu, kwota_kredytu - pozostalo_kredytu))
+kwota_kredytu = pozostalo_kredytu
+
+# Maj_2
+
+inflacja = Maj_2
+
+pozostalo_kredytu = (1 +((inflacja + wysokosc_oprocentowania) / 1200)) * kwota_kredytu - wysokosc_raty
+tekst = "Twoja pozostala kwota kredytu to {}. To o {} mniej niz w poprzednim miesiacu"
+
+print(tekst.format(pozostalo_kredytu, kwota_kredytu - pozostalo_kredytu))
+kwota_kredytu = pozostalo_kredytu
+
+# Czerwiec_2
+
+inflacja = Czerwiec_2
+
+pozostalo_kredytu = (1 +((inflacja + wysokosc_oprocentowania) / 1200)) * kwota_kredytu - wysokosc_raty
+tekst = "Twoja pozostala kwota kredytu to {}. To o {} mniej niz w poprzednim miesiacu"
+
+print(tekst.format(pozostalo_kredytu, kwota_kredytu - pozostalo_kredytu))
+kwota_kredytu = pozostalo_kredytu
+
+# Lipiec_2
+
+inflacja = Lipiec_2
+
+pozostalo_kredytu = (1 +((inflacja + wysokosc_oprocentowania) / 1200)) * kwota_kredytu - wysokosc_raty
+tekst = "Twoja pozostala kwota kredytu to {}. To o {} mniej niz w poprzednim miesiacu"
+
+print(tekst.format(pozostalo_kredytu, kwota_kredytu - pozostalo_kredytu))
+kwota_kredytu = pozostalo_kredytu
+
+# Sierpien_2
+
+inflacja = Sierpien_2
+
+pozostalo_kredytu = (1 +((inflacja + wysokosc_oprocentowania) / 1200)) * kwota_kredytu - wysokosc_raty
+tekst = "Twoja pozostala kwota kredytu to {}. To o {} mniej niz w poprzednim miesiacu"
+
+print(tekst.format(pozostalo_kredytu, kwota_kredytu - pozostalo_kredytu))
+kwota_kredytu = pozostalo_kredytu
+
+# Wrzesien_2
+
+inflacja = Wrzesien_2
+
+pozostalo_kredytu = (1 +((inflacja + wysokosc_oprocentowania) / 1200)) * kwota_kredytu - wysokosc_raty
+tekst = "Twoja pozostala kwota kredytu to {}. To o {} mniej niz w poprzednim miesiacu"
+
+print(tekst.format(pozostalo_kredytu, kwota_kredytu - pozostalo_kredytu))
+kwota_kredytu = pozostalo_kredytu
+
+
+# Pazdziernik_2
+
+inflacja = Pazdziernik_2
+
+pozostalo_kredytu = (1 +((inflacja + wysokosc_oprocentowania) / 1200)) * kwota_kredytu - wysokosc_raty
+tekst = "Twoja pozostala kwota kredytu to {}. To o {} mniej niz w poprzednim miesiacu"
+
+print(tekst.format(pozostalo_kredytu, kwota_kredytu - pozostalo_kredytu))
+kwota_kredytu = pozostalo_kredytu
+
+# Listopad_2
+
+inflacja = Listopad_2
+
+pozostalo_kredytu = (1 +((inflacja + wysokosc_oprocentowania) / 1200)) * kwota_kredytu - wysokosc_raty
+tekst = "Twoja pozostala kwota kredytu to {}. To o {} mniej niz w poprzednim miesiacu"
+
+print(tekst.format(pozostalo_kredytu, kwota_kredytu - pozostalo_kredytu))
+kwota_kredytu = pozostalo_kredytu
+
+# Grudzien_2
+
+inflacja = Grudzien_2
+
+pozostalo_kredytu = (1 +((inflacja + wysokosc_oprocentowania) / 1200)) * kwota_kredytu - wysokosc_raty
+tekst = "Twoja pozostala kwota kredytu to {}. To o {} mniej niz w poprzednim miesiacu"
+
+print(tekst.format(pozostalo_kredytu, kwota_kredytu - pozostalo_kredytu))
+kwota_kredytu = pozostalo_kredytu
+
